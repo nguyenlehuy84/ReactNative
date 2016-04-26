4f7c232c488ffa9c57e381d1b527f43c5fe8feda
huy.ng <huy.nguyenle@smartdev.vn>
2016-04-26 15:01:30 +0700

    react native router

---
+++ MainApp.js
@@ -0,0 +1,60 @@
+import React, {AppRegistry, Navigator, StyleSheet, Text, View} from 'react-native'
+import {Scene, Reducer, Router, Switch, TabBar, Modal, Schema, Actions} from 'react-native-router-flux'
+import Launch from './components/Launch'
+import Login from './components/Login'
+import Login2 from './components/Login2'
+import Error from './components/Error'
+import Register from './components/Register'
+class TabIcon extends React.Component {
+    render(){
+        return (
+            <Text style={{color: this.props.selected ? "red" :"black"}}>{this.props.title}</Text>
+        );
+    }
+}
+
+class Right extends React.Component {
+    render(){
+        return <Text style={{
+        width: 80,
+        height: 37,
+        position: "absolute",
+        bottom: 4,
+        right: 2,
+        padding: 8,
+    }}>Right</Text>
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {flex:1, backgroundColor:"transparent",justifyContent: "center",
+        alignItems: "center",}
+
+});
+
+const reducerCreate = params=>{
+    const defaultReducer = Reducer(params);
+    return (state, action)=>{
+        console.log("ACTION:", action);
+        return defaultReducer(state, action);
+    }
+};
+
+export default class MainApp extends React.Component {
+    render() {
+        return <Router createReducer={reducerCreate}>
+            <Scene key="modal" component={Modal} >
+                <Scene key="root"  hideNavBar={true} >   
+                    <Scene key="launch" component={Launch} title="Launch" initial={true}  />
+                    <Scene key="register" hideNavBar={false}  component={Register} title="Register"/>
+                    <Scene key="register2" component={Register} title="Register2" duration={1}/>
+                    <Scene key="login" direction="vertical"  >
+                        <Scene key="loginModal" component={Login} title="Login" backTitle="back" duration={100}/>
+                        <Scene key="loginModal2" hideNavBar={true} component={Login2} title="Login2" panHandlers={null} duration={100}/>
+                    </Scene>
+                </Scene>
+                <Scene key="error" component={Error}/>
+            </Scene>
+        </Router>;
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var React = require('react-native');
+var {AppRegistry, Navigator, StyleSheet,Text,View} = React;
+import Huysample from './MainApp';
+AppRegistry.registerComponent('Huysample', () => Huysample);
